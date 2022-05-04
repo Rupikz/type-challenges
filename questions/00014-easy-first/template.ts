316b253c7def4cd4569124f1cf5cfb8f0eee1c68
rupikz <kydry.nikolau@gmail.com>
2022-05-04 23:40:20 +0300

    [EASY] resolved 14

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,6 @@
-type First<T extends any[]> = any
+// My solution
+type First<T extends any[]> = T['length'] extends 0 ? never : T[0]
+
+// Other solution
+// type First1<T extends any[]> = T extends [] ? never : T[0]
+// type First<T extends any[]> = T extends [infer A, ...infer X] ? A : never
