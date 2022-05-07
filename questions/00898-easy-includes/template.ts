db2948ee431bf78dc10c664a8602b93c5f1da75d
rupikz <kydry.nikolau@gmail.com>
2022-05-07 11:30:34 +0300

    [EASY] resolved 898, 3057, 3060, 3312

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,5 @@
-type Includes<T extends readonly any[], U> = any
+import type { Equal } from '@type-challenges/utils'
+
+export type Includes<T extends readonly any[], U> = T extends [infer A, ...infer REST]
+  ? Equal<A, U> extends true ? true : Includes<REST, U>
+  : false
--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,1 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => void> = T extends (...args: infer U) => void ? U : undefined
