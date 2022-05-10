d77ac4f3ebe9e2e37651a1c83afd4a10c6463b19
rupikz <kydry.nikolau@gmail.com>
2022-05-10 12:44:23 +0300

    [MEDIUM] resolved 15, 16, 106

--- questions/00106-medium-trimleft/template.ts
+++ questions/00106-medium-trimleft/template.ts
@@ -1,1 +1,8 @@
-type TrimLeft<S extends string> = any
+type TrimLeft<S extends string> = S extends `${infer F}${infer G}`
+  ? F extends ' ' | '\n' | '\t' ? TrimLeft<G> : `${F}${G}`
+  : S
+
+// solution from answer
+
+// type TrimLeft<S extends string> = S extends `${' ' | '\n' | '\t'}${infer G}`
+//   ? TrimLeft<G> : S
