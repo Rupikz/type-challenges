b2370d4637a287506b5c2e97b5f7da0e7227c8bc
rupikz <kydry.nikolau@gmail.com>
2022-05-13 11:00:38 +0300

    [MEDIUM] 599,1042

--- questions/00599-medium-merge/template.ts
+++ questions/00599-medium-merge/template.ts
@@ -1,1 +1,3 @@
-type Merge<F, S> = any
+type Merge<F extends Record<string, unknown>, S extends Record<string, unknown>>
+    = { [KEY in (keyof F | keyof S)]: KEY extends keyof S ? S[KEY] :
+      KEY extends keyof F ? F[KEY] : never }
